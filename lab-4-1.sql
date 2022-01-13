5f273a118aea97285b805baa643e756dc872d672
hinakilam <hina.malik@kellogg.northwestern.edu>
2022-01-13 03:30:06 +0000

    Practicing sql

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,7 @@
 -- Expected result:
 -- 2935
 
-
+select sum(stats.hits) 
+from players inner join stats on players.id = stats.player_id
+where first_name = "Barry"
+and last_name = "Bonds";
---
+++ school-6-update.sql
@@ -0,0 +1,2 @@
+update students
+set email = 
